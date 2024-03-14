1bfa0fa5e3f6612e10d4e1e13b6369c33aaa23ce
MWorksCoding <mail.org@posteo.org>
2024-03-14 12:56:39 +0100

    first working verion

---
+++ todo/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.conf import settings
+from datetime import datetime
+
+class Todo(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)
+    created_at = models.DateTimeField(default=datetime.now)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE
+    )
+    
